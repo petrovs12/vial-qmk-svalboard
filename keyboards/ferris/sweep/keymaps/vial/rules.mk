9eaa519248101dea3034489fdfe73fede144792e
stefanpetrov <stefanpetrov@meta.com>
2023-12-31 12:36:34 +0100

    changes to sweep commit

--- keyboards/ferris/sweep/keymaps/vial/rules.mk
+++ keyboards/ferris/sweep/keymaps/vial/rules.mk
@@ -4,6 +4,11 @@ LTO_ENABLE = yes
 QMK_SETTINGS = yes
 # EXTRAFLAGS += -flto
 
+AUTO_SHIFT_ENABLE   = yes
+TAP_DANCE_ENABLE    = yes
+RETRO_TAPPING_ENABLE = yes
+RETRO_SHIFT_ENABLE  = yes
+t
 
 RGBLIGHT_ENABLE     = no
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
